3ff875cb994b84b9a2fb64e4d2fbb353724d6b51
Lailson Bandeira <lailson@guava.com.br>
2011-06-26 09:19:25 -0300

    Função subconjunto_estratificado alterada para funcionar em conjunto com cvpartition.

--- subconjunto_estratificado.m
+++ subconjunto_estratificado.m
@@ -1,47 +1,21 @@
+%  X   - matriz de dados
+%  C   - classes
 %  k   - número do conjunto k-fold
-%  n   - número total de conjuntos
+%  c   - cvpartition
 % Ts   - conjunto de teste
 % Tr   - conjunto de treinamento
 % C_tr - classes do conjunto de treinamento
-% C_tr - classes do conjunto de teste
-function [Tr Ts C_tr C_ts] = subconjunto_estratificado(k, n, varargin)
-  if(k > n)
-    error('k nao pode ser maior que n.');
+% C_ts - classes do conjunto de teste
+function [Tr Ts C_tr C_ts] = subconjunto_estratificado(X, C, k, c)
+  if(k > c.NumTestSets)
+    error('k nao pode ser maior que o número de conjuntos.');
   end
   
-  d = size(varargin{1}, 2);
-  c = size(varargin, 2);
-  N = zeros(k, 1);
-  for i = 1:c
-    N(i) = size(varargin{i}, 1);
-  end
+  TrIndices = find(c.training(k));
+  TsIndices = find(c.test(k));
   
-  n_ts = sum(N/n);
-  n_tr = sum(N) - n_ts;
-  
-  Tr   = zeros(n_tr, d);
-  Ts   = zeros(n_ts, d);
-  C_tr = zeros(n_tr, 1);
-  C_ts = zeros(n_ts, 1);
-  
-  i_tr = 1;
-  i_ts = 1;
-  for i = 1:c
-    X = varargin{i};
-    
-    qtd = N(i)/n;
-    inicio = (k-1)*qtd + 1;
-    fim = inicio + qtd - 1;
-    
-    fim_ts = i_ts + qtd - 1;
-    Ts(i_ts:fim_ts, :) = X(inicio:fim, :);
-    C_ts(i_ts:fim_ts) = repmat(i, fim_ts-i_ts+1, 1);
-    i_ts = fim_ts + 1;
-    
-    fim_tr = i_tr + N(i) - qtd - 1;
-    X(inicio:fim, :) = [];
-    Tr(i_tr:fim_tr, :) = X;
-    C_tr(i_tr:fim_tr) = repmat(i, fim_tr-i_tr+1, 1);
-    i_tr = fim_tr + 1;
-  end
+  Tr = X(TrIndices, :);
+  Ts = X(TsIndices, :);
+  C_tr = C(TrIndices);
+  C_ts = C(TsIndices);
 end
