5b925fdd1b4f7b8fdb623cb01241f7fbc2bcb96f
Emanuel Ferreira <emanuel.b.a.ferreira@gmail.com>
2011-06-27 09:46:40 -0300

    * Completada segunda questão.

--- questao_2.m
+++ questao_2.m
@@ -5,29 +5,39 @@ carregar_dados;
 carregar_cluster;
 
 % ---------------------------------------------
-% PARAMETROS
-% ---------------------------------------------
-H = [1, 2]; % TODO Mudar valores
-Kernels = {'normal', 'janela'};
-
-
-% ---------------------------------------------
 % CLASSIFICAÇÃO COM DISCRIMINANTES AMOSTRAIS
 % ---------------------------------------------
 disp('Classificando com discriminantes amostrais...');
 [C_da Pb_da] = da_classificador(X, X, C_eq);
 imprimir_avaliacao(C_da, C_eq);
 
-
 % ---------------------------------------------
 % CLASSIFICAÇÃO COM JANELA DE PARZEN
 % ---------------------------------------------
 disp('Classificando com janela de Parzen...');
-for kernel = Kernels
-  for h = H
-    disp(sprintf('kernel = %s', kernel{1}));
-    disp(sprintf('h = %d', h));
-    [C_pw Pb_pw_i] = da_classificador(X, X, C_eq); % TODO mudar para pw
-    imprimir_avaliacao(C_pw, C_eq);
-  end
-end
+
+disp(sprintf('kernel = normal'));
+[C_pw1 Pb_pw1] = pw_classificador(X, X, C_eq, 'normal');
+imprimir_avaliacao(C_pw1, C_eq);
+
+disp(sprintf('kernel = janela'));
+[C_pw2 Pb_pw2] = pw_classificador(X, X, C_eq, 'janela');
+imprimir_avaliacao(C_pw2, C_eq);
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM SOMA
+% ---------------------------------------------
+[C_cs Pb_cs] = combinacao_soma_classificador(Pb_da, Pb_pw1, Pb_pw2);
+imprimir_avaliacao(C_cs, C_eq);
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM PRODUTO
+% ---------------------------------------------
+[C_cp Pb_cp] = combinacao_produto_classificador(Pb_da, Pb_pw1, Pb_pw2);
+imprimir_avaliacao(C_cp, C_eq);
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM MEDIANA
+% ---------------------------------------------
+[C_cm Pb_cm] = combinacao_mediana_classificador(Pb_da, Pb_pw1, Pb_pw2);
+imprimir_avaliacao(C_cm, C_eq);
