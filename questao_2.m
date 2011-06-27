60467c6e150438add0b305abeecd5f7336eac393
Lailson Bandeira <lailson@guava.com.br>
2011-06-26 22:22:03 -0300

    Correção da questao 2.

--- questao_2.m
+++ questao_2.m
@@ -4,30 +4,41 @@
 carregar_dados;
 carregar_cluster;
 
-% ---------------------------------------------
-% PARAMETROS
-% ---------------------------------------------
-H = [1, 2]; % TODO Mudar valores
-Kernels = {'normal', 'janela'};
-
 
 % ---------------------------------------------
 % CLASSIFICAÇÃO COM DISCRIMINANTES AMOSTRAIS
 % ---------------------------------------------
 disp('Classificando com discriminantes amostrais...');
 [C_da Pb_da] = da_classificador(X, X, C_eq);
-imprimir_avaliacao(C_da, C_eq);
+imprimir_taxa_erro(C_da, C_eq);
 
 
 % ---------------------------------------------
 % CLASSIFICAÇÃO COM JANELA DE PARZEN
 % ---------------------------------------------
 disp('Classificando com janela de Parzen...');
-for kernel = Kernels
-  for h = H
-    disp(sprintf('kernel = %s', kernel{1}));
-    disp(sprintf('h = %d', h));
-    [C_pw Pb_pw_i] = da_classificador(X, X, C_eq); % TODO mudar para pw
-    imprimir_avaliacao(C_pw, C_eq);
-  end
-end
+
+disp(sprintf('kernel = normal'));
+[C_pw Pb_pw_i] = da_classificador(X, X, C_eq); % TODO mudar para pw
+imprimir_taxa_erro(C_pw, C_eq);
+
+disp(sprintf('kernel = janela'));
+[C_pw Pb_pw_i] = da_classificador(X, X, C_eq); % TODO mudar para pw
+imprimir_taxa_erro(C_pw, C_eq);
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM SOMA
+% ---------------------------------------------
+% TODO
+
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM PRODUTO
+% ---------------------------------------------
+% TODO
+
+
+% ---------------------------------------------
+% CLASSIFICAÇÃO COM MEDIANA
+% ---------------------------------------------
+% TODO
